569c8c6756c5c649beb0c78a2f8146f8996c2814
RoGo47 <145758910+RoGo47@users.noreply.github.com>
2024-09-11 10:06:05 +0200

    Update zweites_File.cpp

--- zweites_File.cpp
+++ zweites_File.cpp
@@ -1,26 +1,3 @@
-name: C++ CI
-
-on:
-  push:
-    branches:
-      - main
-  pull_request:
-    branches:
-      - main
-
-jobs:
-  build:
-    runs-on: ubuntu-latest
-
-    steps:
-    - name: Checkout repository
-      uses: actions/checkout@v2
-
-    - name: Install dependencies
-      run: sudo apt-get update && sudo apt-get install -y g++
-
-    - name: Build the application
-      run: g++ -o Erstes_Repository zweites_File.cpp
-
-    - name: Run the application
-      run: ./Erstes_Repository
+int main(){
+  
+}
